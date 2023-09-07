ee0634979395ca3fa8db8e2589f1e9aa15018eff
phq <895514136@qq.com>
2023-09-07 15:31:39 +0800

    上传hRPC项目

--- Consumer/src/main/java/com/phq/Consumer.java
+++ Consumer/src/main/java/com/phq/Consumer.java
@@ -6,8 +6,13 @@ public class Consumer {
 
     public static void main(String[] args) {
 
+        //消费者consumer 远程调用 供应者provider的接口helloService.sayHello()
+        //The consumer remotely invokes the interface of the provider helloService.sayHello()
         HelloService helloService = ProxyFactory.getProxy(HelloService.class);
-        String result = helloService.sayHello("hanqing123123。。。。。。123");
+
+        //在这里写下你想传入的参数内容
+        //Write down the parameters you want to pass in here
+        String result = helloService.sayHello("Write down the parameters you want to pass in here");
         System.out.println(result);
 
     }
